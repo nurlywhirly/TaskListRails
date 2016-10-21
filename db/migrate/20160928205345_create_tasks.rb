b138e4787b206321f0179782b6b0158c6ebb30bd
Nicole Url <nicole.url.4@gmail.com>
2016-10-20 22:31:20 -0700

    generated new migration to add a user_id column to tasks

--- db/migrate/20160928205345_create_tasks.rb
+++ db/migrate/20160928205345_create_tasks.rb
@@ -1,6 +1,7 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
+      t.belongs_to :user
       t.string :name
       t.string :description
       t.date :completed
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017213941) do
+ActiveRecord::Schema.define(version: 20161021050520) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161017213941) do
     t.date     "completed_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
