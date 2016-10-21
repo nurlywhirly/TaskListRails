5eab9368ae42c3e7596466b28a5653c69e3726cc
Nicole Url <nicole.url.4@gmail.com>
2016-10-20 22:32:06 -0700

    Added associations - a user has_many tasks, a task belongs_to user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  belongs_to :user
+
   def self.all_completes
     task_list = Task.all.where.not(completed_at: nil)
 
@@ -9,6 +11,12 @@ class Task < ActiveRecord::Base
     task_list = Task.all.where(completed_at: nil)
 
     return task_list
+  end
+
+  def user_tasks_only(records)
+    records.delete_if do |record|
+      record.user_id != session[:user_id]
+    end
   end
 
 end
