923cf1c6aea9b33c6d4a11bd127edcc0b7a69579
Nicole Url <nicole.url.4@gmail.com>
2016-10-22 19:39:57 -0700

    Changed methods in model to not be self methods. Pulled new index instance variable into tasks/index.html.erb. Changed login link to not be an <li>

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,3 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-
-  def self.all_completes
-    task_list = Task.all.where.not(completed_at: nil)
-
-    return task_list
-  end
-
-  def self.all_incompletes
-    task_list = Task.all.where(completed_at: nil)
-
-    return task_list
-  end
-
-  def user_tasks_only(records)
-    records.delete_if do |record|
-      record.user_id != session[:user_id]
-    end
-  end
-
 end
